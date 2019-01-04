0da6d9aa72fd508b909241fdd540d122cb064f15
brettlangdon <brett.langdon@datadoghq.com>
2019-01-03 20:00:29 -0500

    be more defensive

--- ddtrace/utils/log.py
+++ ddtrace/utils/log.py
@@ -7,8 +7,18 @@ def format_log_message(msg, *args, **kwargs):
     if args:
         msg = msg % args
 
-    pid = os.getpid()
-    thread = threading.current_thread()
-    process = multiprocessing.current_process()
+    pid = None
+    pname = None
+    tname = None
+    tid = None
+    try:
+        pid = os.getpid()
+        thread = threading.current_thread()
+        tname = thread.name
+        tid = threading.get_ident()
+        process = multiprocessing.current_process()
+        pname = process.name
+    except Exception:
+        pass
 
-    return '({}:{}) ({}:{}) {}'.format(process.name, pid, thread.name, threading.get_ident(), msg)
+    return '({}:{}) ({}:{}) {}'.format(pname, pid, tname, tid, msg)
