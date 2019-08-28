024b5a4da6b1c8e611a180830444f3b1cda31306
Alban Crequy <alban@kinvolk.io>
2019-08-28 16:46:10 +0200

    [development] add script and example application

---
+++ examples/helloflask.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    r1 = requests.get(url = "http://localhost:8055/word1")
+    r2 = requests.get(url = "http://localhost:8055/word2")
+    return 'Hello ' + r1.text + ' ' + r2.text
+
+@app.route('/word1')
+def hello_word1():
+    r1 = requests.get(url = "http://localhost:8055/word1/details")
+    return 'Alice ' + r1.text
+
+@app.route('/word1/details')
+def hello_word1_details():
+    return 'Wonderland'
+
+@app.route('/word2')
+def hello_word2():
+    return 'Bob'
+
+app.run(host='127.0.0.1', port=8055)
