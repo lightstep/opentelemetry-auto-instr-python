0f874e090f4e116780cc37371eb88f81afe23360
Alex Boten <aboten@lightstep.com>
2019-11-18 16:41:51 -0800

    Add Apache license to source code
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- oteltrace/internal/runtime/constants.py
+++ oteltrace/internal/runtime/constants.py
@@ -1,3 +1,17 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 GC_COUNT_GEN0 = 'runtime.python.gc.count.gen0'
 GC_COUNT_GEN1 = 'runtime.python.gc.count.gen1'
 GC_COUNT_GEN2 = 'runtime.python.gc.count.gen2'
--- oteltrace/utils/hook.py
+++ oteltrace/utils/hook.py
@@ -1,3 +1,17 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 This module is based off of wrapt.importer (wrapt==1.11.0)
 https://github.com/GrahamDumpleton/wrapt/blob/4bcd190457c89e993ffcfec6dad9e9969c033e9e/src/wrapt/importer.py#L127-L136
